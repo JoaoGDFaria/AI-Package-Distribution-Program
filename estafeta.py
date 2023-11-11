fd59962b45b3dcec5c4eabcf69bc8887a80ebf32
JoaoGDFaria <joaofaria13@gmail.com>
2023-11-11 16:36:27 +0000

    I cooked

--- estafeta.py
+++ estafeta.py
@@ -1,5 +1,8 @@
 import info
 import encomenda
+import random
+import Graph as g
+from itertools import permutations
 
 class Estafeta:
 
@@ -14,6 +17,11 @@ class Estafeta:
         self.perdaPorKg = info.infoPerdaPorKg[veiculo]
         self.disponivel = True
 
+    # Função para determinar o tempo de entrega de uma encomenda
+    def tempoEntrega(self, encomenda):
+        tempo = 0
+
+
 
     # Função para determinar a penalização do rating de um estafeta
     # com base no seu atraso em horas
@@ -23,22 +31,44 @@ class Estafeta:
             self.ranking = 0
             
         return ranking
-        
-        
-            
-            
-        
+
+    def melhorCaminho(self, locaisentrega):
+        valor=0
+        temp=0
+        all_permutations = list(permutations(locaisentrega))
+        for i in all_permutations:
+            temp = self.pesoCaminho(i)
+            if temp < valor:
+                valor = temp
+        return valor
+
+    def pesoCaminho(self, locaisentrega):
+        valor = 0
+        for i in locaisentrega:
+            valor += g.procura_BFS(self.localizacao, i)
+            self.localizacao = i
+        return valor
 
 
     # Fazer uma determianda entrega com base nas encomendas a entregar
-    def fazerEntrega(self, encomendas, locaisEntrega):
+    def fazerEntrega(self, encomendas, locaisEntrega, metereologia):
         self.disponivel = False
         pesoTotalEncomendas = sum(encomendas.peso)
-        self.velocidadeMaxima -= self.perdaPorKg * pesoTotalEncomendas
-
-
+        self.velocidadeMedia -= self.perdaPorKg * pesoTotalEncomendas
+        if metereologia == False and (self.veiculo == "bicicleta" or self.veiculo == "moto"): # False = chuva, veiculos que sofrem penalizacao
+            self.velocidadeMedia*=0.8
+        num = random.randint(1,100)
+        rand = random.randint(1,80)
+        rand /= 100
+        if self.veiculo == "carro" and num < 10:
+            self.velocidadeMedia *= rand
+        elif self.veiculo == "moto" and num < 20:
+            self.velocidadeMedia *= rand
+        elif self.veiculo == "bicicleta" and num < 30:
+            self.velocidadeMedia *= rand
 
         self.velocidadeMaxima = info.infoVelocidadeMedia[self.veiculo]
+        self.localizacao = locaisEntrega[-1]
         self.disponivel = True
 
 
