fcb9ef8f4bde32630aee55a6dcfb2c77ba9f8688
hramos04 <116158805+hramos04@users.noreply.github.com>
2023-11-11 13:58:15 +0000

    penalizacao estafeta

--- encomenda.py
+++ encomenda.py
@@ -1,13 +1,18 @@
+import info
 class Encomenda:
 
-    def __init__(self, idEncomenda, peso, volume, localizacao, idCliente):
+    def __init__(self, idEncomenda, idEstafeta, peso, volume,localizacao, idCliente, tempoInicio, tempoPedido, tempo):
         self.idEncomenda = idEncomenda
         self.idCliente = idCliente
+        self.idEstafeta = idEstafeta
         self.peso = peso
         self.volume = volume
         self.precoBase = 5 * self.volume / self.peso
         self.localizacao = localizacao
         self.determinarVeiculo()
+        self.tempoInicio = tempoInicio
+        self.tempoPedido = tempoPedido
+        self.tempo = tempo
 
 
     # Determinar estafeta a utilizar
@@ -30,3 +35,29 @@ class Encomenda:
         # Carro
         elif self.veiculo == "carro":
             self.precoBase *= 1.5
+
+    # Função para determinar a penalização em função
+    # do tipo de veículo, estado de tempo e distância         
+    def tempoEntrega(self, id, distancia):
+        veiculo = x
+        condicoesTransporte = info.infoTempo[[self.tempo], veiculo]
+        
+        penalizacao = condicoesTransporte * distancia
+        
+        return penalizacao
+        
+           
+            
+    # Função para determinar a penalização de uma encomenda
+    # com base no seu atraso em minutos
+    def penalizacaoEncomenda(self, tempoEntrega, id, distancia):
+        tempoUtil = int (tempoEntrega - self.tempoInicio)/60
+        
+        if(tempoUtil >= self.tempoPedido):
+            tempoMinutos = tempoUtil - self.tempoPedido
+            penalizacaoEncomenda = tempoMinutos * tempoEntrega(self, id, distancia)
+
+        return penalizacaoEncomenda
+             
+           
+        
--- estafeta.py
+++ estafeta.py
@@ -1,13 +1,16 @@
 import info
+import encomenda
 
 class Estafeta:
 
-    def __init__(self, veiculo, id):
+    def __init__(self, veiculo, id, localizacao):
         self.veiculo = veiculo
         self.id = id
         self.ranking = 0
+        self.atraso = 0
+        self.localizacao = localizacao
         self.pesoMaximo = info.infoPesoMaximo[veiculo]
-        self.velocidadeMaxima = info.infoVelocidadeMedia[veiculo]
+        self.velocidadeMedia = info.infoVelocidadeMedia[veiculo]
         self.perdaPorKg = info.infoPerdaPorKg[veiculo]
         self.disponivel = True
 
@@ -15,7 +18,16 @@ class Estafeta:
     # Função para determinar a penalização do rating de um estafeta
     # com base no seu atraso em horas
     def penalizacaoEstafeta(self):
-        pass
+        ranking = self.ranking - encomenda.penalizacaoEncomenda
+        if(ranking < 0):
+            self.ranking = 0
+            
+        return ranking
+        
+        
+            
+            
+        
 
 
     # Fazer uma determianda entrega com base nas encomendas a entregar
